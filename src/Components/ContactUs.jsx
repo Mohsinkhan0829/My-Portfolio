4f2317c55d10a509eb5f015099f6e6358fdb47ef
unknown <research.mohsinkhan@gmail.com>
2023-10-31 16:50:58 +0530

    i change contact public key

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -10,14 +10,14 @@ const ContactUs = () => {
     const sendEmail = (e) => {
       e.preventDefault();
 
-      emailjs.sendForm('service_jfe52ql', 'template_dsahb0r', form.current, 'NoSUewWql5TFJJu-Z')
+      emailjs.sendForm('service_jfe52ql', 'template_dsahb0r', form.current, 'Uytp_j9krTxRiCdU4')
         .then((result) => {
             console.log(result.text);
             setDone(true);
         }, (error) => {
             console.log(error.text);
         });
-
+        
     };
   
   
@@ -31,7 +31,7 @@ const ContactUs = () => {
 
         <div className='Contact-right'>
             <form ref={form} onSubmit={sendEmail}>
-                <input type="text" name='user_name' className='user' placeholder='Name' required />
+                <input type="text" name='from_name' className='user' placeholder='Name' required />
                 <input type="email" name='user_email' className='user' placeholder='Email' required/>
                 <textarea name="message" cols="30" rows="5" className='user' placeholder='Massage' required></textarea>
                 <input type="submit" value="Send-Massage" className='button' />
